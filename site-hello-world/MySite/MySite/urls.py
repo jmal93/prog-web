808d40b30c4b5af699c4a9a82a7d1d7c7830c1e3
João Marcello <marcello.jao1611@gmail.com>
2025-09-02 08:54:38 -0300

    Second page

--- site-hello-world/MySite/MySite/urls.py
+++ site-hello-world/MySite/MySite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from MeuApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='homepage'),
+    path('SegundaPagina', views.segundaPagina, name='segunda'),
 ]
